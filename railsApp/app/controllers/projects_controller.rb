b7b1f719741d467734cbafd81a04277ff2aab2dc
Eamon Ito-fisher <eito-fis@e1z3r1p1.42.us.org>
2017-06-26 15:18:07 -0700

    Front end work

--- railsApp/app/controllers/projects_controller.rb
+++ railsApp/app/controllers/projects_controller.rb
@@ -8,14 +8,39 @@ class ProjectsController < ApplicationController
 	end
 
 	def new
+		@project = Project.new
+	end
+
+	def edit
+		@project = Project.find(params[:id])
 	end
 
 	def create
 		@project = Project.new(project_params)
 
-		@project.save
-		redirect_to @project
-	end	
+		if @project.save
+    		redirect_to @project
+		else
+    		render 'new'
+ 		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update(project_params)
+			redirect_to @project
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+
+		redirect_to projects_path
+	end
 
 	private
 		def project_params
